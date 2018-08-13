fa5721e0ec1cb64d88615e4759112888d7a9031a
Ben Hall <Ben@Benhall.me.uk>
2018-08-13 14:52:54 +0100

    Update code.sh

--- cake/code.sh
+++ cake/code.sh
@@ -1,7 +1,5 @@
 echo "Setting up Cake Environment"
 cd /root
 echo 'Information("Hello from Cake");' > build.cake
-docker pull devlead/gitlabs-dotnet
-docker tag devlead/gitlabs-dotnet cake:latest
-alias cake="docker run -it --rm -v /root:/cake -w /cake devlead/gitlabs-dotnet cake"
+alias cake="docker run -it --rm -v /root:/cake -w /cake cake cake"
 clear
